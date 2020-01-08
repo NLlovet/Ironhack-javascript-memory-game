80bdc704ea3c39b326be2205f94342be3065bd32
Llovet Nicholas <nllovet20199@columbushs.com>
2020-01-08 13:19:28 -0500

    start

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -2,7 +2,11 @@ class MemoryGame {
   constructor(cards){
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = cards;
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
+  
   shuffleCards() {}
   checkIfPair(card1, card2) {}
   isFinished() {}
